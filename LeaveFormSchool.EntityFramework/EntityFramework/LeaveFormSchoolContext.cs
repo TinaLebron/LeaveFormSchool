063ff3e51b17916ee39283d2cb2418b85ad4108a
LebronTina <Tina.Lin@infortrend.com>
2021-01-26 20:19:30 +0800

    initial project

---
+++ LeaveFormSchool.EntityFramework/EntityFramework/LeaveFormSchoolContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeaveFormSchool.EntityFramework.EntityFramework
+{
+    public class LeaveFormSchoolContext : IdentityDbContext<IdentityUser>
+    {
+        public LeaveFormSchoolContext() : base("LeaveFormSchoolContext")
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
